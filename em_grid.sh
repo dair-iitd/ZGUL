8ee5344e61c524bb20ca0e27c26abba135cbd81c
rathorevipul28 <rathorevipul28@gmail.com>
2023-10-12 23:10:33 +0530

    Update em_grid.sh

--- em_grid.sh
+++ em_grid.sh
@@ -5,10 +5,7 @@ for step in 1 5 10;
 do
 for LANG in de is;
 do
-#bash test_pos_emea.sh $step $lr
-#bash test_pos_emea.sh $1 $step $lr 3
-bash test_ner_emea.sh $LANG $step $lr 3
-bash test_pos_emea1.sh $LANG $step $lr 3
+bash test_pos_emea.sh $LANG $step $lr 3
 done
 done
 done
